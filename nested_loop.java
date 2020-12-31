271646406ee0692dde022148cabb06066b3f1e10
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-31 18:27:04 +0200

    Update nested_loop.java

--- nested_loop.java
+++ nested_loop.java
@@ -55,6 +55,17 @@ public class Main
         // 6   7  8  9 10
         // 11 12 13 14 15
         // 16 17 18 19 20
+        // 3
+        // *etgar:
+        // input from user number of rows
+        // input from user number of columns
+        // print the following:
+        // i.e. 2 rows and 5 column:
+        // print row x column (not multiply, just letter 'x'), hint:use format
+        // attention: not zero based
+        // 1x1 1x2 1x3 1x4 1x5
+        // 2x1 2x2 2x3 2x9 2x10        
+        
         
     }
 }
